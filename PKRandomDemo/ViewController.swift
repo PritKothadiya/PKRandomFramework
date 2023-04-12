368950c1dfb18ffa80a28ee63d60a61e00466e81
Prit Kothadiya <prithkothadiya@Aurionpros-MacBook-Pro.local>
2023-04-12 11:08:23 +0530

    First commit

---
+++ PKRandomDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PKRandomDemo
+//
+//  Created by Prit Kothadiya on 12/04/23.
+//
+
+import UIKit
+//import PKRandomFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //RandomStruct.randomFunc()
+    }
+
+
+}
+
---
+++ PKRandomFramework.xcframework/ios-arm64/PKRandomFramework.framework/Headers/PKRandomFramework.h
@@ -0,0 +1,18 @@
+//
+//  PKRandomFramework.h
+//  PKRandomFramework
+//
+//  Created by Prit Kothadiya on 12/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PKRandomFramework.
+FOUNDATION_EXPORT double PKRandomFrameworkVersionNumber;
+
+//! Project version string for PKRandomFramework.
+FOUNDATION_EXPORT const unsigned char PKRandomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKRandomFramework/PublicHeader.h>
+
+
